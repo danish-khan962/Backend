6d470d37fb19a9d3485a41747c39baa5604ab97e
Danish Khan <dk831675@gmail.com>
2025-05-24 13:48:28 +0530

    minor changes

--- 05_CRUD/frontend/src/components/Update.jsx
+++ 05_CRUD/frontend/src/components/Update.jsx
@@ -25,7 +25,7 @@ const Update = () => {
   const { id } = useParams();
   const getUser = async () => {
     try {
-      const response = await axios.get(`http://localhost/4000/${id}`);
+      const response = await axios.get(`http://localhost:4000/${id}`);
       const result = response.data;
 
       console.log(result);
