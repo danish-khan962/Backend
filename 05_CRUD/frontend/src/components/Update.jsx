b3d05ac83f7af901fcbbbbe85e5ce33bce7a6f79
Danish Khan <dk831675@gmail.com>
2025-05-24 13:47:28 +0530

    minor changes

--- 05_CRUD/frontend/src/components/Update.jsx
+++ 05_CRUD/frontend/src/components/Update.jsx
@@ -25,7 +25,7 @@ const Update = () => {
   const { id } = useParams();
   const getUser = async () => {
     try {
-      const response = await axios.get(`https://crud-backend-jet-ten.vercel.app/${id}`);
+      const response = await axios.get(`http://localhost/4000/${id}`);
       const result = response.data;
 
       console.log(result);
